e46f3d12dad9257300b6e74e2e9f898bae391d27
Константин Мироненко <mironenko.ka@yandex.ru>
2025-02-23 20:34:21 +0600

    edit form validation

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -52,7 +52,9 @@ export async function createInvoice(prevState: State, formData: FormData) {
       VALUES (${customerId}, ${amountInCents}, ${status}, ${date})
   `;
   } catch (error) {
-    console.log(error)
+    return {
+      message: 'Database Error: Failed to Create Invoice.',
+    };
   }
 
 
@@ -63,13 +65,19 @@ export async function createInvoice(prevState: State, formData: FormData) {
 // Use Zod to update the expected types
 const UpdateInvoice = FormSchema.omit({ id: true, date: true });
 
-export async function updateInvoice(id: string, formData: FormData) {
-  const { customerId, amount, status } = UpdateInvoice.parse({
+export async function updateInvoice(id: string, prevState: State, formData: FormData) {
+  const validatedFields = UpdateInvoice.safeParse({
     customerId: formData.get('customerId'),
     amount: formData.get('amount'),
     status: formData.get('status'),
-  });
-
+  })
+  if (!validatedFields.success) {
+    return {
+      errors: validatedFields.error.flatten().fieldErrors,
+      message:  'Missing Fields. Failed to Create Invoice.',
+    }
+  }
+  const { customerId, amount, status } = validatedFields.data;
   const amountInCents = amount * 100;
   try {
     await sql`
@@ -79,7 +87,9 @@ export async function updateInvoice(id: string, formData: FormData) {
   `;
 
   } catch (error) {
-    console.log(error)
+    return {
+      message: 'Database Error: Failed to Update Invoice.',
+    };
   }
 
   revalidatePath('/dashboard/invoices');
